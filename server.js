dac5d3399acf601c9bb62fce0775b1dada9a6cf2
chandrashekhar mathuria <csmathuria03@gmail.com>
2017-05-30 14:44:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ app.get('/article-one',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
 });
 app.get('/article-two',function(req,res){
-   res.send('Article two will be served here');
+     res.sendFile(path.join(__dirname, 'ui', 'article_two.html'));
+  
 });
 app.get('/article-three',function(req,res){
-   res.send('Article three will be served here');
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
